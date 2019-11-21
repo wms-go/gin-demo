f0b1e3df4deac5073d3995c218f773eb9a1f1d3e
wumingsheng <mingsheng.wu@qingclass.com>
2019-11-21 15:11:39 +0800

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/wms-go/gin-demo
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.4.0 // indirect
+require github.com/gin-gonic/gin v1.4.0
--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 
+	engine := gin.Default()
+	engine.GET("ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 
+
+	})
+
+	engine.Run()
 
 }
